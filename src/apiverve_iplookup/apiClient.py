0d14ec39a8393f2015dd7aad29b7ae827e82fa1b
APIVerve Team <hello@apiverve.com>
2025-03-28 17:27:39 -0500

    Bug Fixes

--- src/apiverve_iplookup/apiClient.py
+++ src/apiverve_iplookup/apiClient.py
@@ -10,10 +10,10 @@ class IplookupAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/iplookup'
+        self.base_url = 'https://api.apiverve.com/v1/iplookup'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
